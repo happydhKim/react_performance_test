91db35a6ec95e27967258edef1ce9640ff3e805d
happydhKim <happytime870616@gmail.com>
2022-03-05 19:39:53 +0900

    [Setting] Routes 초기 세팅 - #3

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,20 @@
-const {
-  useBabelRc,
-  removeModuleScopePlugin,
-  override,
-} = require('customize-cra')
+const path = require('path');
+const { override, getBabelLoader } = require('customize-cra');
 
-module.exports = override(
-  useBabelRc(),
-  removeModuleScopePlugin(),
-);
+const removeConfig = (config) => {
+  const loader = getBabelLoader(config);
+
+  loader.options.presets = [];
+  loader.options.plugins = [];
+
+  return config;
+};
+
+const enableBabelConfig = (config) => {
+  const loader = getBabelLoader(config);
+  loader.options.configFile = path.resolve(__dirname, 'babel.config.js');
+
+  return config;
+};
+
+module.exports = override(removeConfig, enableBabelConfig);
