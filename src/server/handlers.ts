48cdefbe996351a920c18080104e4a460191ffbf
happydhKim <happytime870616@gmail.com>
2022-03-11 23:16:09 +0900

    [Feature] mock api 생성 - #12

--- src/server/handlers.ts
+++ src/server/handlers.ts
@@ -2,6 +2,10 @@ import { rest } from 'msw';
 
 export const handlers = () => {
   return [rest.get('/api/user', getUserInformation)];
+  return [rest.get('/api/first', getFirstInfo)];
+  return [rest.get('/api/second', getSecondInfo)];
+  return [rest.get('/api/third', getThirdInfo)];
+  return [rest.get('/api/fourth', getFourthInfo)];
 };
 
 const getUserInformation: Parameters<typeof rest.get>[1] = (_, res, ctx) => {
@@ -10,6 +14,42 @@ const getUserInformation: Parameters<typeof rest.get>[1] = (_, res, ctx) => {
     ctx.json({
       name: '김도현',
       age: 7,
+    })
+  );
+};
+
+const getFirstInfo: Parameters<typeof rest.get>[1] = (_, res, ctx) => {
+  return res(
+    ctx.status(200),
+    ctx.json({
+      description: '첫 번째 테스트....',
+    })
+  );
+};
+
+const getSecondInfo: Parameters<typeof rest.get>[1] = (_, res, ctx) => {
+  return res(
+    ctx.status(200),
+    ctx.json({
+      description: '두 번째 테스트....',
+    })
+  );
+};
+
+const getThirdInfo: Parameters<typeof rest.get>[1] = (_, res, ctx) => {
+  return res(
+    ctx.status(200),
+    ctx.json({
+      description: '세 번째 테스트....',
+    })
+  );
+};
+
+const getFourthInfo: Parameters<typeof rest.get>[1] = (_, res, ctx) => {
+  return res(
+    ctx.status(200),
+    ctx.json({
+      description: '네 번째 테스트....',
     })
   );
 };
