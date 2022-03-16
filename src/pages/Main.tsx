e0a0aaf61636d76f07ae3779f9b39e80f6f07d11
happydhKim <happytime870616@gmail.com>
2022-03-16 23:34:40 +0900

    [Feature] default config 추가, axios 설치 - #18

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -3,19 +3,19 @@ import { useQuery } from 'react-query';
 import type { FC } from 'react';
 
 const Main: FC = () => {
-  const { isLoading: firstLoading, data: first, isError, error } = useQuery('first', () =>
+  const { isLoading: firstLoading, data: first, isError, error } = useQuery(['first'], () =>
     fetch('/api/first').then(res => res.json()),
-    {retry: 3},
+    { retry: 5 },
   );
-  const { isLoading: secondLoading, data: second } = useQuery('second', () =>
+  const { isLoading: secondLoading, data: second } = useQuery(['second', first], () =>
     fetch(`/api/second?depth=${first.depth}`).then(res => res.json()),
     { enabled: !!first, retry: 3 },
   );
-  const { isLoading: thirdLoading, data: third } = useQuery('third', () =>
+  const { isLoading: thirdLoading, data: third } = useQuery(['third', second], () =>
     fetch(`/api/third?depth=${second.depth}`).then(res => res.json()),
     { enabled: !!second, retry: 3 },
   );
-  const { isLoading: fourthLoading, data: fourth } = useQuery('fourth', () =>
+  const { isLoading: fourthLoading, data: fourth } = useQuery(['fourth', third], () =>
     fetch(`/api/fourth?depth=${third.depth}`).then(res => res.json()),
     { enabled: !!third, retry: 3 },
   );
